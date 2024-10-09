b3f88b8142e33c7c1c9044a3c536a440e49ee68d
TacosConChelas <adriherandezor@gmail.com>
2024-10-09 09:33:31 -0600

    Se ha agregado el agregar contacto y el buscar y eliminar contacto

--- EjerciciosPractica/Dia1/Ejer4.java
+++ EjerciciosPractica/Dia1/Ejer4.java
@@ -1,15 +1,10 @@
 package EjerciciosPractica.Dia1;
-
 import java.util.Scanner;
 import java.util.Random;
-
-
 /**
  * Ejer 4
  * Crea un programa en Java que simule un juego de piedra, papel o tijera. El usuario debe elegir una de las opciones 
  * y el programa debe elegir aleatoriamente una opción para enfrentarse al usuario, mostrando el resultado del juego.
-
-
  */
 
 public class Ejer4 {
@@ -26,15 +21,15 @@ public class Ejer4 {
                 if (usuario == maquina) {
                     mensaje = "Empate";
                 } else if ((usuario == 1)) {
-                    mensaje = (maquina == 2)? ("Has perido, ya que la maquina ha elegido PAPEL") : ("Has ganado ya que la maquina ha elegido TIJERA");
+                    mensaje = (maquina == 2)? ("Has perido, ya que la maquina ha elegido PAPEL") : ("Has ganado, ya que la maquina ha elegido TIJERA");
                 } else if ((usuario == 2)) {
-                    mensaje = (maquina == 3)? ("Has perido, ya que la maquina ha elegido TIJERA") : ("Has ganado ya que la maquina ha elegido PIEDRA");
+                    mensaje = (maquina == 3)? ("Has perido, ya que la maquina ha elegido TIJERA") : ("Has ganado, ya que la maquina ha elegido PIEDRA");
                 } else {
-                    mensaje = (maquina == 1)? ("Has perido, ya que la maquina ha elegido PIEDRA") : ("Has ganado ya que la maquina ha elegido PIEDRA");
+                    mensaje = (maquina == 1)? ("Has perido, ya que la maquina ha elegido PIEDRA") : ("Has ganado, ya que la maquina ha elegido PIEDRA");
                 } System.out.println(mensaje);
             } else {    System.out.println("Lo sentimos pero has escrito mal la opción");}
+            System.out.println("Selecciona si quieres jugar o no: \n0) Si \n1) No");    opcion = leer.nextInt();
         }
         // System.out.println(random.nextInt((3 - 1) + 1) + 1);
-
     }
 }
--- EjerciciosPractica/Dia1/Ejer5.java
+++ EjerciciosPractica/Dia1/Ejer5.java
@@ -1,5 +1,66 @@
 package EjerciciosPractica.Dia1;
+/**
+ * Ejer 5
+ * Implementa un programa en Java que gestione una agenda de contactos. El programa debe permitir al usuario añadir,
+ * eliminar, buscar y mostrar contactos, almacenando la información en una lista y utilizando clases para representar los contactos.
+ */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class Ejer5 {
+    static List<Contacto> listaContactos = new ArrayList<>();
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+        boolean salir = true; byte opcion;
+        do{
+            System.out.println("Selecciona la opçión que decees realizar: \n1) Agregar un nuevo contacto \n2) Elimininar contactos");
+            System.out.println("3) Buscar algun contacto \n4) Salir del sistema");
+            opcion = leer.nextByte();
+            switch (opcion) {
+                case 1:
+                    Contacto contacto = new Contacto();
+                    System.out.println("Ingresa el nombre del contacto");
+                    contacto.setNameUser(leer.nextLine());
+                    System.out.println("Coloca el número de teléfono del contacto");
+                    contacto.setPhoneNumberUser(leer.nextLong(10));
+                    System.out.println("Coloca la dirección del contacto");
+                    contacto.setAddressUser(leer.nextLine());
+                    listaContactos.add(contacto);
+                    break;
+                case 2: 
+                    System.out.println("Escribe el nombre de la persona que deceas eliminar");
+                    buscarContactoYEliminar(listaContactos, leer.nextLine());
+                    
+                    break;
+                case 3: 
+                    break;
+                case 4: salir = false;
+                    break;
+                default:
+                    break;
+            }
+        } while(salir);
+    }
+    public void mostrarListaDContactos(List<Contacto> listaContacto){
+
+    }
+    public static void buscarContactoYEliminar(List<Contacto> listaContacto, String name){
+        boolean encontrado = false;
+        for (Contacto contacto : listaContacto) {
+            if (name.equals(contacto.getNameUser())) {
+                encontrado = true;
+                listaContacto.remove(contacto);
+            }
+        }
+        if (encontrado) {   System.out.println("Elemento eliminado");   } else {    System.out.println("Elemento no encontrado");}
+    }
+    public void buscarYMostrarContacto(List<Contacto> listaContacto){
+        
+    }
     
 }
