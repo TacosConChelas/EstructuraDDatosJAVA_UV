aa4557899a57f504678dc031be174ad463c6b32b
TacosConChelas <adriherandezor@gmail.com>
2024-10-08 23:10:25 -0600

    prueba 1

--- EjerciciosPractica/Dia1/Ejer4.java
+++ EjerciciosPractica/Dia1/Ejer4.java
@@ -3,7 +3,6 @@ package EjerciciosPractica.Dia1;
 import java.util.Scanner;
 import java.util.Random;
 
-import javax.security.sasl.SaslException;
 
 /**
  * Ejer 4
@@ -16,12 +15,24 @@ import javax.security.sasl.SaslException;
 public class Ejer4 {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);  Random random = new Random();
-        byte usuario, maquina;
+        int usuario, maquina; String mensaje = "";
         System.out.println("Selecciona si quieres jugar o no: \n0) Si \n1) No");
         int opcion = leer.nextInt();
         while (opcion == 0) {
-            System.out.println("Ingresa un número dependiendo de tu elección: \n1) Piedra \n");
-
+            System.out.println("Ingresa un número dependiendo de tu elección: \n1) Piedra \n2) Papel \n3) Tijera");
+            usuario = leer.nextInt();
+            maquina = random.nextInt((3 - 1) + 1) + 1;
+            if ((usuario >= 1) && (usuario <= 3)) {
+                if (usuario == maquina) {
+                    mensaje = "Empate";
+                } else if ((usuario == 1)) {
+                    mensaje = (maquina == 2)? ("Has perido, ya que la maquina ha elegido PAPEL") : ("Has ganado ya que la maquina ha elegido TIJERA");
+                } else if ((usuario == 2)) {
+                    mensaje = (maquina == 3)? ("Has perido, ya que la maquina ha elegido TIJERA") : ("Has ganado ya que la maquina ha elegido PIEDRA");
+                } else {
+                    mensaje = (maquina == 1)? ("Has perido, ya que la maquina ha elegido PIEDRA") : ("Has ganado ya que la maquina ha elegido PIEDRA");
+                } System.out.println(mensaje);
+            } else {    System.out.println("Lo sentimos pero has escrito mal la opción");}
         }
         // System.out.println(random.nextInt((3 - 1) + 1) + 1);
 
