3bdb2babcba1526044699b88c4c807a0b9c55ec0
TacosConChelas <adriherandezor@gmail.com>
2024-10-08 22:49:25 -0600

    Prueba 1 con el ejercicio 4

--- EjerciciosPractica/Dia1/Ejer4.java
+++ EjerciciosPractica/Dia1/Ejer4.java
@@ -1,5 +1,10 @@
 package EjerciciosPractica.Dia1;
 
+import java.util.Scanner;
+import java.util.Random;
+
+import javax.security.sasl.SaslException;
+
 /**
  * Ejer 4
  * Crea un programa en Java que simule un juego de piedra, papel o tijera. El usuario debe elegir una de las opciones 
@@ -10,6 +15,15 @@ package EjerciciosPractica.Dia1;
 
 public class Ejer4 {
     public static void main(String[] args) {
-        
+        Scanner leer = new Scanner(System.in);  Random random = new Random();
+        byte usuario, maquina;
+        System.out.println("Selecciona si quieres jugar o no: \n0) Si \n1) No");
+        int opcion = leer.nextInt();
+        while (opcion == 0) {
+            System.out.println("Ingresa un número dependiendo de tu elección: \n1) Piedra \n");
+
+        }
+        System.out.println(random.nextInt((3 - 1) + 1) + 1);
+
     }
 }
