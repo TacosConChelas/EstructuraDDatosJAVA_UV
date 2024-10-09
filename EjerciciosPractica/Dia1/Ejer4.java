1bde2890312ffe700e88553e9d6bc1571fed3231
TacosConChelas <adriherandezor@gmail.com>
2024-10-08 22:33:29 -0600

    ejer 4 establecido

--- EjerciciosPractica/Dia1/Ejer4.java
+++ EjerciciosPractica/Dia1/Ejer4.java
@@ -1,5 +1,15 @@
 package EjerciciosPractica.Dia1;
 
+/**
+ * Ejer 4
+ * Crea un programa en Java que simule un juego de piedra, papel o tijera. El usuario debe elegir una de las opciones 
+ * y el programa debe elegir aleatoriamente una opción para enfrentarse al usuario, mostrando el resultado del juego.
+
+
+ */
+
 public class Ejer4 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
